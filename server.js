ef12cc67b37de58c9baf4a7b37e0079db0676632
Jamie Nadel <jamienadel4@gmail.com>
2023-04-09 13:03:02 -0400

    adding body parser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const path = require('path');
@@ -7,6 +8,8 @@ const app = express();
 
 const PORT = 3001;
 
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
@@ -17,6 +20,14 @@ app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', (err, data) => {
     if (err) throw err;
     res.send(JSON.parse(data));
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  console.log('req.body', req.body);
+  fs.writeFile('./db/db.json', JSON.stringify(req.body), (err) => {
+    if (err) throw err;
+    console.log('req.body', req.body);
   });
 });
 
